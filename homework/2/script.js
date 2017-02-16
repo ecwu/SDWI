2d58f286087a22e834e9026d6dfa59c054958f30
ecwu <bitlabwzh@gmail.com>
2017-02-16 20:25:36 +0800

    update

--- homework/2/script.js
+++ homework/2/script.js
@@ -1,3 +1,71 @@
-function mulitjobs(){
-	document.getElement
+function ainfo(){
+	var userName = document.getElementById("username").value;
+
+	function skills(){
+/*	var selecta = document.getElementById('skilla');
+	var selectb = document.getElementById('skillb')
+	var selectc = document.getElementById('skillc')*/
+	var selecta = document.getElementById('skilla');
+	var selecta = document.getElementById('skillb');
+	var selecta = document.getElementById('skillc');
+	var result = [];
+	var input;
+		for (var i = 1, iLen = select.length; i<iLen; i++){
+			input = select[i];
+			if (input.selected){
+				jobresult.push(" " + input.value);
+			}
+		}
+		return result; 
+	}
+
+	function jobsdata(){
+	var select = document.getElementById('multiple');
+	var result = [];
+	var option;
+		for (var i = 0, iLen = select.length; i<iLen; i++){
+			option = select[i];
+			if (option.selected){
+				jobresult.push(" " + option.value);
+			}
+		}
+		return result;
+	}
+
+/*alert("User Name: " + userName);*/
+alert("Array: " + skills());
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function jobs(select) {
+	var result = [];
+	var options = select && select.options;
+	var opt;
+	for (var i=0, iLen=options.length; i<iLen; i++) {
+		opt = options[i];
+		if (opt.selected) {
+			result.push(opt.value || opt.text);
+		}
+	}
+	return result;
+}*/
