d5e101720ed98e901a7ef67872b56af25d546504
ecwu <bitlabwzh@gmail.com>
2017-02-21 21:08:31 +0800

    a basic calculator

--- homework/3/js/script.js
+++ homework/3/js/script.js
@@ -1,42 +1,65 @@
 /**
  * Created by ecwu on 2/21/17.
  */
-function numbtnone(){
-
+function numBtnOne(){
+	document.getElementById("display").value = document.getElementById("display").value + "1";
 }
 
-function numbtntwo(){
-	
+function numBtnTwo(){
+	document.getElementById("display").value = document.getElementById("display").value + "2";
 }
 
-function numbtnthree(){
-	
+function numBtnThree(){
+	document.getElementById("display").value = document.getElementById("display").value + "3";
 }
 
-function numbtnfour(){
-	
+function numBtnFour(){
+	document.getElementById("display").value = document.getElementById("display").value + "4";
 }
 
-function numbtnfive(){
-	
+function numBtnFive(){
+	document.getElementById("display").value = document.getElementById("display").value + "5";
 }
 
-function numbtnsix(){
-	
+function numBtnSix(){
+	document.getElementById("display").value = document.getElementById("display").value + "6";
 }
 
-function numbtnseven(){
-	
+function numBtnSeven(){
+	document.getElementById("display").value = document.getElementById("display").value + "7";
 }
 
-function numbtneight(){
-	
+function numBtnEight(){
+	document.getElementById("display").value = document.getElementById("display").value + "8";
 }
 
-function numbtnnine(){
-	
+function numBtnNine(){
+	document.getElementById("display").value = document.getElementById("display").value + "9";
 }
 
-function numbtnzero(){
-	
+function numBtnZero(){
+	document.getElementById("display").value = document.getElementById("display").value + "0";
+}
+
+function operationPlus(){
+	document.getElementById("display").value = document.getElementById("display").value + "+";
+}
+
+function operationMinus(){
+	document.getElementById("display").value = document.getElementById("display").value + "-";
+}
+
+function operationMultiplication(){
+	document.getElementById("display").value = document.getElementById("display").value + "*";
+}
+
+function operationDivision(){
+	document.getElementById("display").value = document.getElementById("display").value + "/";
+}
+
+function calOutput(){
+	display = document.getElementById("display").value;
+	if (display != ""){
+		document.getElementById("display").value = eval(display);
+	}
 }
