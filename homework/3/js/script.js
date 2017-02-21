dd407e9eed8946a66beb2f79c2c0c6377febe5ab
ecwu <bitlabwzh@gmail.com>
2017-02-21 17:41:03 +0800

    update js and html

--- homework/3/js/script.js
+++ homework/3/js/script.js
@@ -1,3 +1,42 @@
 /**
  * Created by ecwu on 2/21/17.
  */
+function numbtnone(){
+
+}
+
+function numbtntwo(){
+	
+}
+
+function numbtnthree(){
+	
+}
+
+function numbtnfour(){
+	
+}
+
+function numbtnfive(){
+	
+}
+
+function numbtnsix(){
+	
+}
+
+function numbtnseven(){
+	
+}
+
+function numbtneight(){
+	
+}
+
+function numbtnnine(){
+	
+}
+
+function numbtnzero(){
+	
+}
