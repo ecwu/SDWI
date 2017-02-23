8d964b87f8a0282786f2573e1db99b02f9fc0d65
ecwu <bitlabwzh@gmail.com>
2017-02-23 11:21:41 +0800

    add some function

--- homework/3/js/script.js
+++ homework/3/js/script.js
@@ -18,6 +18,48 @@ function constantCe(){
 	document.getElementById("display").value = "";
 	document.getElementById("equation").value = "";
 }
+function functionLogE(){
+	var display = document.getElementById("display").value;
+	document.getElementById("equation").value = "ln" + "(" + display + ")=";
+	document.getElementById("display").value = Math.log(eval(display));
+}
+function functionLogTen(){
+	var display = document.getElementById("display").value;
+	document.getElementById("equation").value = "log" + "(" + display + ")=";
+    document.getElementById("display").value = Math.log(eval(display))/Math.log(10);
+}
+function functionSqrt(){
+    var display = document.getElementById("display").value;
+    document.getElementById("equation").value = "√" + "(" + display + ")=";
+    document.getElementById("display").value = Math.sqrt(eval(display));
+}
+function functionSin(){
+    var display = document.getElementById("display").value;
+    document.getElementById("equation").value = "sin" + "(" + display + ")=";
+    document.getElementById("display").value = Math.sin(eval(display));
+}
+function functionCos(){
+    var display = document.getElementById("display").value;
+    document.getElementById("equation").value = "cos" + "(" + display + ")=";
+    document.getElementById("display").value = Math.cos(eval(display));
+}
+function functionTan(){
+    var display = document.getElementById("display").value;
+    document.getElementById("equation").value = "tan" + "(" + display + ")=";
+    document.getElementById("display").value = Math.tan(eval(display));
+}
+function functionFactor(){
+    var display = document.getElementById("display").value;
+    var number = eval(display);
+    var output = 1;
+    if (number === 0){
+    	for (var i=1; i<number; i++){
+			output = output * i;
+		}
+	}
+    document.getElementById("equation").value ="(" + display + ")!=";
+    document.getElementById("display").value = output;
+}
 function calOutput(){
 	equaled = 1;
 	var display = document.getElementById("display").value;
