007470453d31148890442f0fce40f3e81b585d64
ecwu <bitlabwzh@gmail.com>
2017-02-23 09:51:23 +0800

    update javascript code

--- homework/3/js/script.js
+++ homework/3/js/script.js
@@ -1,34 +1,28 @@
-/**
- * Created by ecwu on 2/21/17.
- */
-
 π = Math.PI;
 e = Math.E;
+/*assign two math constant*/
 equaled = 0;
+/*assign a var to clean the screen*/
 function equalTest(){
 	if (equaled == 1){
 		constantCe();
 		equaled = 0;
 	}
 }
-
-
 function writeInfo(chars){
 	equalTest();
 	char = chars;
 	document.getElementById("display").value = document.getElementById("display").value + char;
 }
-
 function constantCe(){
 	document.getElementById("display").value = "";
 	document.getElementById("equation").value = "";
 }
-
 function calOutput(){
+	equaled = 1;
 	var display = document.getElementById("display").value;
 	if (display != ""){
-		document.getElementById("equation").value = display;
+		document.getElementById("equation").value = display+"=";
 		document.getElementById("display").value = eval(display);
-		equaled = 1;
 	}
 }
