2c499883f2a794752791251f47e8d79b5e1e7778
ecwu <bitlabwzh@gmail.com>
2017-02-24 11:19:31 +0800

    empty display error

--- homework/3/js/script.js
+++ homework/3/js/script.js
@@ -9,6 +9,13 @@ function equalTest(){
 		equaled = 0;
 	}
 }
+function emptyTest(){
+    if (document.getElementById("display").value == ""){
+        document.getElementById("display").value = "EMPTY ERROR";
+        equaled = 1;
+    }
+}
+
 function writeInfo(chars){
 	equalTest();
 	char = chars;
@@ -19,36 +26,42 @@ function constantCe(){
 	document.getElementById("equation").value = "";
 }
 function functionLogE(){
-	var display = document.getElementById("display").value;
+    var display = document.getElementById("display").value;
 	document.getElementById("equation").value = "ln" + "(" + display + ")=";
 	document.getElementById("display").value = Math.log(eval(display));
 }
 function functionLogTen(){
-	var display = document.getElementById("display").value;
+    emptyTest();
+    var display = document.getElementById("display").value;
 	document.getElementById("equation").value = "log" + "(" + display + ")=";
     document.getElementById("display").value = Math.log(eval(display))/Math.log(10);
 }
 function functionSqrt(){
+    emptyTest();
     var display = document.getElementById("display").value;
     document.getElementById("equation").value = "√" + "(" + display + ")=";
     document.getElementById("display").value = Math.sqrt(eval(display));
 }
 function functionSin(){
+    emptyTest();
     var display = document.getElementById("display").value;
     document.getElementById("equation").value = "sin" + "(" + display + ")=";
     document.getElementById("display").value = Math.sin(eval(display));
 }
 function functionCos(){
+    emptyTest();
     var display = document.getElementById("display").value;
     document.getElementById("equation").value = "cos" + "(" + display + ")=";
     document.getElementById("display").value = Math.cos(eval(display));
 }
 function functionTan(){
+    emptyTest();
     var display = document.getElementById("display").value;
     document.getElementById("equation").value = "tan" + "(" + display + ")=";
     document.getElementById("display").value = Math.tan(eval(display));
 }
 function functionFactor(){
+    emptyTest();
     var display = document.getElementById("display").value;
     var output = 1;
     if (eval(display)%1 === 0){
@@ -60,11 +73,13 @@ function functionFactor(){
     document.getElementById("display").value = output;
 }
 function functionTwoTimes(){
+    emptyTest();
     var display = document.getElementById("display").value;
     document.getElementById("equation").value ="(" + display + ")^2=";
     document.getElementById("display").value = eval(display*display);
 }
 function functionXTimes(){
+    emptyTest();
     var display = document.getElementById("display").value;
     var times=prompt("y=","");
     var output = 1;
@@ -76,6 +91,7 @@ function functionXTimes(){
 }
 function calOutput(){
 	equaled = 1;
+    emptyTest();
 	var display = document.getElementById("display").value;
 	if (display != ""){
 		document.getElementById("equation").value = display+"=";
