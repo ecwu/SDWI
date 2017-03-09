36dab0bf28f0aef0555e5155360cdc799a215bfa
ecwu <bitlabwzh@gmail.com>
2017-03-09 23:07:55 +0800

    update

--- homework/5/script.js
+++ homework/5/script.js
@@ -11,6 +11,7 @@ var seats=[[false,false,false,false,false,false],
     [true,true,true,true,true,true],
     [true,true,true,true,true,true],
     [true,true,true,true,true,true]];
+var submit=[];
 function seatInit(){
     var postion = 0;
     for (var i = 0; i < seats.length; i += 1){
@@ -30,6 +31,7 @@ function seatInit(){
 }
 function seatSelect(seatid){
     var img = document.getElementById(seatid).alt;
+    seatId = seatid.substring(4,seatid.length);
     if (img == "os"){
         alert("This seat have been occupied!")
     }else{
@@ -40,14 +42,44 @@ function seatSelect(seatid){
                 document.getElementById(seatid).src = "img/seleted_seats.png";
                 document.getElementById(seatid).alt = "ss";
                 selectSeats += 1;
+                seatArrayInfoChange(false,seatid);
+                submit.push(seatId);
             }
         }else{
             document.getElementById(seatid).src = "img/normal_seats.png";
             document.getElementById(seatid).alt = "as";
             selectSeats -= 1;
+            seatArrayInfoChange(true,seatid);
+            var index = submit.indexOf("seatId");
+            submit.splice(index,1);
         }
     }
 }
+function seatArrayInfoChange(type,id){
+    var seatId = id;
+    var seatNumber = seatId.substring(4,seatId.length);
+    var j = (seatNumber%6);
+    var i = (seatNumber-j)/6;
+    if (type){
+        seats[i].splice(j-1,1,true)
+    }else{
+        seats[i].splice(j-1,1,false)
+    }
+}
 function seatSubmit(){
-
+    var seatName = [];
+    var colName = ["A","B","C","J","K","L"];
+    var rowName = [11,12,13,14,15,16,17,18,19,20,21,22];
+    for (var a = 0; a < submit.length; a += 1){
+        var num = submit[a];
+        var j = (num%6);
+        var i = (num-j)/6;
+        if (j == 0){
+            j = 6;
+        }
+        seatName.push(rowName[i]+colName[j-1]);
+    }
+    if (confirm("You are Selecting: "+ seatName + "\nAre you sure?")){
+        seatInit()
+    }
 }
