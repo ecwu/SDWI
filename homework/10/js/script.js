2c3924ce8e2d8abf30e625b59f389c1f84389f78
ecwu <bitlabwzh@gmail.com>
2017-04-20 10:10:02 +0800

    update

--- homework/10/js/script.js
+++ homework/10/js/script.js
@@ -74,3 +74,63 @@ function finalCheck() {
         setTimeout(function(){ document.getElementById('reminder').innerHTML = '';},2000);
     }
 }
+
+
+function generateyear(){
+    getid("year").innerHTML="<option value=''>YYYY</option>";
+    for(var i=2017;i>1900;i--){
+        getid("year").innerHTML+="<option value='"+i+"'>"+i+"</option>";
+    }
+}
+
+function generatemonth(){
+    getid("month").innerHTML="<option value=''>MM</option>";
+    for(var i=1;i<=12;i++){
+        getid("month").innerHTML+="<option value='"+i+"'>"+i+"</option>";
+    }
+}
+var thisyear=0;
+var thismonth=0;
+function getyearmon(element,judge) {
+    if(judge) {
+        thisyear = parseInt(element.value);
+    }
+    else{
+        thismonth=parseInt(element.value);
+    }
+}
+function generateday(month,year){// check if the feb have 29 days
+    var two=28;
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            daylength(31);
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            daylength(30);
+            break;
+        default:
+            if(year%4==0){
+                two=29;
+            }
+            daylength(two);
+    }
+
+}
+function daylength(long){//input the day into the select box
+    getid("day").innerHTML="<option value=''>DD</option>";
+    for(var i=1;i<=long;i++){
+        getid("day").innerHTML+="<option value='"+i+"'>"+i+"</option>";
+    }
+}
+function getid(element){//get element
+    return document.getElementById(element);
+}
